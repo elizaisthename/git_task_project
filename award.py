e852d9ca9498d55e8d7a7ab7089b8edef2423891
Eliza <elizapalos7@gmail.com>
2024-03-24 14:23:41 +0000

    added_

--- award.py
+++ award.py
@@ -1,13 +1,13 @@
 # asking the user to input the swimming time
-swim_time = int(input("Enter the time taken for swimming, in minutes, please: "))
+swim_time_ = int(input("Enter the time taken for swimming, in minutes, please: "))
 
 # asking the user to input the cycling time
-cycle_time = int(input("Enter the time taken for cycling, in minutes, please: "))
+cycle_time_ = int(input("Enter the time taken for cycling, in minutes, please: "))
 
 # asking the user to input the running time
-run_time = int(input("Enter the time taken for running, in minutes, please: "))
+run_time_ = int(input("Enter the time taken for running, in minutes, please: "))
 
-total_minutes = swim_time+cycle_time+run_time
+total_minutes = swim_time_+cycle_time_+run_time_
 
 if total_minutes <= 100:
     print("Provincial colours")
