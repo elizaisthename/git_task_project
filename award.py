0192a014eb4d3fb313572fe8279ceb4c1f8037d3
Eliza <elizapalos7@gmail.com>
2024-03-24 14:01:30 +0000

    Changed variables

---
+++ award.py
@@ -0,0 +1,19 @@
+# asking the user to input the swimming time
+swim_time = int(input("Enter the time taken for swimming, in minutes, please: "))
+
+# asking the user to input the cycling time
+cycle = int(input("Enter the time taken for cycling, in minutes, please: "))
+
+# asking the user to input the running time
+run = int(input("Enter the time taken for running, in minutes, please: "))
+
+total_minutes = swim_time+cycle+run
+
+if total_minutes <= 100:
+    print("Provincial colours")
+elif total_minutes <= 105:
+    print("Provincial Half Colours")
+elif total_minutes <= 110:
+    print("Provincial Scroll")
+elif total_minutes >= 111:
+    print("No award")
